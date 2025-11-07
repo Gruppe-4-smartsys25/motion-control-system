ea0a18c055deac7cf7f5f54b87a4f11846525ff1
aasmundwt <aasmundwt@hotmail.com>
2025-11-07 03:05:50 +0100

    stuffn things, it's late...

--- Compass.py
+++ Compass.py
@@ -2,14 +2,15 @@
 """
 Created on Mon Nov  3 16:22:50 2025
 
-@author: guy in forum: https://forums.raspberrypi.com/viewtopic.php?t=123737
-(and Åsmund a little)
-"""
+@author: Åsmund
 
-"""
+Sources:
+Starting point: https://forums.raspberrypi.com/viewtopic.php?t=123737
 From microbit lsm303 driver: https://github.com/lancaster-university/microbit-dal/blob/master/source/drivers/LSM303Magnetometer.cpp
-And LSM303AGR datasheet: https://www.st.com/resource/en/datasheet/lsm303agr.pdf
+LSM303AGR datasheet: https://www.st.com/resource/en/datasheet/lsm303agr.pdf
 
+
+how microbit reads the compass:
 config
 CFG_REG_A_M = 0x00/0x04/0x08/0x0C  -only setting ODR, 10/20/50/100Hz
 CFG_REG_C_M = 0x01 -configures the INT_MAG/DRDY as a digital output, not something we strictly need
@@ -18,12 +19,10 @@ when reading
 Register: OUTX_L_REG_M | 0x80, the 0x80 sets the MSB high, this enables auto increment
     (LSM303AGR datasheet, 6.1.1 I2C operation, 3rd paragraph)
     
-
 Swaps and inverts x and y, probably to align more sensibly with the microbit
 Also "normalizes" each axis, by multiplying them by 150
 
-
-Need to look more into the calibration done
+appplies calibration
 """
 
 import smbus
@@ -167,14 +166,19 @@ def spherify(c, points):
     
     return (radius, c, [scaleX, scaleY, scaleZ])
     
-#do several data readings and average them
-def readAxisData():
-    sample = readRawAxisData()
-    outX = (sample[0] - calibration_center[0])*calibration_scale[0]
-    outY = (sample[1] - calibration_center[1])*calibration_scale[1]
-    outZ = (sample[2] - calibration_center[2])*calibration_scale[2]
+def readAxisData(n=3):
+    outX = 0
+    outY = 0
+    outZ = 0
     
-    return (outX, outY, outZ)
+    for i in range(n):
+        sample = readRawAxisData()
+        outX += (sample[0] - calibration_center[0])*calibration_scale[0]
+        outY += (sample[1] - calibration_center[1])*calibration_scale[1]
+        outZ += (sample[2] - calibration_center[2])*calibration_scale[2]
+        time.sleep(1/50) #sample at 50Hz
+    
+    return (outX/n, outY/n, outZ/n)
 
 def readRawAxisData():
     xl = bus.read_byte_data(DEVICE_ADDRESS, OUTX_L_REG_M)
@@ -184,8 +188,8 @@ def readRawAxisData():
     zl = bus.read_byte_data(DEVICE_ADDRESS, OUTZ_L_REG_M)
     zh = bus.read_byte_data(DEVICE_ADDRESS, OUTZ_H_REG_M)
 
-    x = _twos_comp(((xh & 0xff)<<8) | xl, 16) # + the x offset. See below.
-    y = _twos_comp(((yh & 0xff)<<8) | yl, 16) # plus the y offset and everything times the y scale. See below.
+    x = _twos_comp(((xh & 0xff)<<8) | xl, 16)
+    y = _twos_comp(((yh & 0xff)<<8) | yl, 16)
     z = _twos_comp(((zh & 0xff)<<8) | zl, 16)
 
     return (x, y, z)
